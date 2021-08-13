b772e7d09db4662a1f297cb081c78b830bd6382d
Jerome <jerome.vial@edu.hefr.ch>
2021-08-13 12:10:28 +0200

    wip animation

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import classes from './Education.module.css';
 import ScrollAnimation from 'react-animate-on-scroll';
-import "animate.css/animate.min.css";
+import "animate.css/animate.compat.css";
 import { MdSchool } from 'react-icons/md';
 import { MdWork } from 'react-icons/md';
 import { FaSchool } from 'react-icons/fa';
@@ -12,14 +12,14 @@ class Education extends Component {
     render() {
         return (
                 <div className={classes.box} id="education">
-                    <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2' animateOnce="true" initiallyVisible ={true}>
+                    <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2' animateOnce="false" initiallyVisible ={true}>
                     <span  className={classes.head}>MY STUDYING JOURNEY</span>
                     <section className={classes.container}>
                         <div className={classes.container_content}>
                             <div className={classes.row}>
                                 <div className={classes.row_md_12}>
                                     <div className={classes.timeline_centered}>
-                                        <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
+                                        <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="false" initiallyVisible ={true}>
                                             <article className={classes.timeline_entry}>
                                                 <div className={`${classes.timeline_icon} ${classes.timeline_icon_2}`} >
                                                     <FaSchool />     
@@ -30,7 +30,7 @@ class Education extends Component {
                                                 </div>
                                             </article>
                                         </ScrollAnimation>
-                                       <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
+                                       <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="false" initiallyVisible ={true}>
                                             <article className={classes.timeline_entry}>
                                                 <div className={classes.timeline_icon} >
                                                 <MdSchool />
@@ -41,7 +41,7 @@ class Education extends Component {
                                                 </div>
                                             </article>
                                         </ScrollAnimation>
-                                        <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
+                                        <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="false" initiallyVisible ={true}>
                                             <article>
                                                 <div className={`${classes.timeline_icon} ${classes.timeline_icon_4}`} >
                                                     <MdWork />     
@@ -64,7 +64,7 @@ class Education extends Component {
                             <div className={classes.row}>
                                 <div className={classes.row_md_12}>
                                     <div className={classes.timeline_centered}>
-                                        <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
+                                        <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="false" initiallyVisible ={true}>
                                             <article className={classes.timeline_entry}>
                                                 <div className={classes.timeline_icon} >
                                                     <GiMeat />     
@@ -75,7 +75,7 @@ class Education extends Component {
                                                 </div>
                                             </article>
                                         </ScrollAnimation>
-                                       <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
+                                       <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="false" initiallyVisible ={true}>
                                             <article className={classes.timeline_entry}>
                                                 <div className={classes.timeline_icon} >
 													<GiSwissArmyKnife />
@@ -86,7 +86,7 @@ class Education extends Component {
                                                 </div>
                                             </article>
                                         </ScrollAnimation>
-                                        <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}>
+                                        <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="false" initiallyVisible ={true}>
                                             <article>
                                                 <div className={classes.timeline_icon} >
                                                     <GiMeat />     
